8f7fbe3e7b6c901a409c37bed60edc64b2febf11
shirlcws <shirlcws@gmail.com>
2023-01-13 17:06:43 -0700

    added media queries

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,1 @@
-  //toLocaleDateString
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-  };
-  document.getElementById('currentdate').textContent =
-    new Date().toLocaleDateString('en-US', options);
+document.getElementById("demo").innerHTML = document.lastModified;
