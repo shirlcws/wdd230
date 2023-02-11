63838c26b81105b852448d4e0b1988b9e351b690
shirlcws <shirlcws@gmail.com>
2023-02-10 19:31:08 -0700

    Added shopping list javascript to bom javascript

--- lesson5/script/bomapps.js
+++ lesson5/script/bomapps.js
@@ -1,45 +1,53 @@
- //three element variables that hold references to the list (<ul>,<input>, and <button> elements.)
- const list = document.querySelector('ul');
- const input = document.querySelector('input');
- const button = document.querySelector('button');
-//Add event listener for add button
- button.addEventListener('click', () => {
-if (input.value !== '') {
-     let chapter = input.value;
+//  //three element variables that hold references to the list (<ul>,<input>, and <button> elements.)
+//  const list = document.querySelector('ul');
+//  const input = document.querySelector('input');
+//  const button = document.querySelector('button');
+// //Add event listener for add button
+//  button.addEventListener('click', () => {
+// if (input.value !== '') {
+//      let chapter = input.value;
 
-    //  input.value = '';
-     const li = document.createElement('li');
-     const deleteBtn = document.createElement('button');
-     
-     li.textContent = chapter;
-     deleteBtn.textContent = '❌';
-     deleteBtn.addEventListener('click'), () => {
-        list.removeChild(li);
-     });
-li.appendChild(deleteBtn);
-     list.appendChild(li);
-    }
-    input.value = '';
-    input.focus();
-});
-    
-   
-//      const myItem = input.value;
 //      input.value = '';
+//      const li = document.createElement('li');
+//      const deleteBtn = document.createElement('button');
+     
+//      li.textContent = chapter;
+//      deleteBtn.textContent = '❌';
+//      deleteBtn.addEventListener('click'), () => {
+//         list.removeChild(li);
+//      });
+// li.appendChild(deleteBtn);
+//      list.appendChild(li);
+//     }
+//     input.value = '';
+//     input.focus();
+// });
+    
+   //  //three element variables that hold references to the list (<ul>,<input>, and <button> elements.)
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+// //Add event listener for add button
+button.addEventListener('click', () => {
+     if (input.value !== '') {
 
-//  const listItem = document.createElement('li');
-//  const listText = document.createElement('span');
-//  const listBtn = document.createElement('button');
+     }
+     let myItem = input.value;
+     input.value = '';
 
-//  listItem.appendChild(listText);
-//  listText.textContent = myItem;
-//  listItem.appendChild(listBtn);
-//  listBtn.textContent ='Delete';
-//  list.appendChild(listItem);
+ const listItem = document.createElement('li');
+ const listText = document.createElement('span');
+ const listBtn = document.createElement('button');
 
-//  listBtn.addEventListener('click', () => {
-//      list.removeChild(listItem);
-//  });
+ listItem.appendChild(listText);
+ listText.textContent = myItem;
+ listItem.appendChild(listBtn);
+ listBtn.textContent ='❌';
+ list.appendChild(listItem);
 
-//  input.focus();
-//  });
+ listBtn.addEventListener('click', () => {
+     list.removeChild(listItem);
+ });
+
+ input.focus();
+ });
