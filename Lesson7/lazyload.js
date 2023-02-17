e995e799b1b52360a6481be338c827db24c9e814
shirlcws <shirlcws@gmail.com>
2023-02-16 22:27:07 -0700

    lesson 7 lazy loading assignment

---
+++ Lesson7/lazyload.js
@@ -0,0 +1,32 @@
+//get imgs attributed as data-src
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// optional parameters for intersectionalObserver
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 500px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+
+};
+//check to see that Intersection Observer is supported
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
