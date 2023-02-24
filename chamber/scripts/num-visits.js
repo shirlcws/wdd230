76cd0282a613baf8f48cb2aaf864cfd0190999da
shirlcws <shirlcws@gmail.com>
2023-02-24 14:07:51 -0700

    Added table build

--- chamber/scripts/num-visits.js
+++ chamber/scripts/num-visits.js
@@ -1,25 +1,32 @@
 //initialize display elements
 // const todayDisplay = document.querySelector("#today");
 const visitsDisplay = document.querySelector("#visits");
+const days = document.querySelector("#days");
+const hrs = document.querySelector("hours");
+const mins = document.querySelector("mins");
+const secs = document.querySelector("secs");
 // const daysLeftOutput = document.querySelector("#daysleft");
-
+let TimeNow = Date.now()
 //get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastVisitTime = Number(window.localStorage.getItem("visits-ls"));
 //Using the Number() function ensures that if the storage item does not exist, it will be converted into a zero (0) which helps
 //on the if block condition.
 
 //determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+if (lastVisitTime !== null) {
+	visitsDisplay.textContent = Math.floor((TimeNow - lastVisitTime)/86400000);
+// days.textContent = (TimeNow %= lastVisitTime)
+	
+
 } else {
 	visitsDisplay.textContent = 'This is your first visit!';
 
 }
 //incremet the number of visits.
-numVisits++;
+// lastVisitTime++;
 
 // store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
+localStorage.setItem("visits-ls", TimeNow );
 
 
 
