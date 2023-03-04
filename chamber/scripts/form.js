6b982fa88c8ab83ea960fb95744f8cbb597733c8
shirlcws <shirlcws@gmail.com>
2023-03-03 23:23:48 -0700

    rearranged message area and removed th space

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -4,6 +4,6 @@
   //To check variables inside object uncomment code below on line 5
 //   console.log(JSON.stringify(objDate));
   
-  // Assign date time string to hidden field value attribute
+  // Assign date time string to hidden field value attribute (shows up in inpector as value)
 document.getElementById('timeStamp').value = objDate.toISOString();
 
