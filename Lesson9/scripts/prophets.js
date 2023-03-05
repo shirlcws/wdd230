e1ae4b16b97f8c5836f689a476d7b3f5efd555ba
shirlcws <shirlcws@gmail.com>
2023-03-05 00:05:53 -0700

    fixed regex pattern issues

--- Lesson9/scripts/prophets.js
+++ Lesson9/scripts/prophets.js
@@ -14,32 +14,44 @@ const displayProphets = (prophets) => {
   const cards = document.querySelector("div.cards"); //selects output container element
 
   prophets.forEach((prophet) => {
-    //Create elements to add to div.cards element
-    let card = document.createElement("section");
-    let h2 = document.createElement("h2");
-    let birthdate = document.createElement("p");
-    let death = document.createElement("p");
-    let portrait = document.createElement("img");
+    //Create elements to add to div.cards element- BirthDate and death are not read
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let death = document.createElement('p');
+    let birthPlace = document.createElement('h3')
+    let lifeSpan = document.createElement('h3')
+    let portrait = document.createElement('img');
 
     //Build the h2 content out to show the prophet's full name - finish the template string
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    
 
-    // ????Build the p content out to show the prophet's birth and death????
-    // p.textContent = '${prophet.birthdate} ${prophet.death}';
+    lifeSpan.textContent = `${prophet.birthdate} - ${prophet.death}`;
+
+    // Build the p content out to show the prophet's birth and place of birth
+    birthDate.textContent = `${prophet.birthdate}`;
+    birthPlace.textContent = `Born in ${prophet.birthplace}`;
+    death.textContent = `${prophet.death}`;
 
     //Build the image protrait by setting all the relevant attribute
-    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute(
-      "alt",
+      'alt',
       `Portait of ${prophet.name} ${prophet.lastname} ${prophet.birthdate} ${prophet.death} ${prophet.length} ${prophet.order} ${prophet.birthplace}`
     );
-    portrait.setAttribute("loading", "lazy");
-    portrait.setAttribute("width", "340");
-    portrait.setAttribute("height", "440");
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
 
     // Append the section(card) with the created elements
     card.appendChild(h2);
+    card.appendChild(birthPlace);
     card.appendChild(portrait);
+    card.appendChild(lifeSpan);
+    // card.appendChild(birthDate);
+    // card.appendChild(death);
+    
 
     cards.appendChild(card);
   }); //end of forEach loop
