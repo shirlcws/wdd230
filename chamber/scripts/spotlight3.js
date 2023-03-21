1c2928b7d9587ffacc09cea4d58142a238bb0ede
shirlcws <shirlcws@gmail.com>
2023-03-21 10:29:47 -0600

    Chamber home pg / sorted 7 spotlights

---
+++ chamber/scripts/spotlight3.js
@@ -0,0 +1,57 @@
+async function fetchAndRenderDirectory() {
+    const dataResponse = await fetch("data.json");
+    const businessData = await dataResponse.json();
+  
+    //take line below out and add console.table line back in to check it in console
+    renderDirectory(businessData.directory);
+    //Use the console.table line to check connection to page
+    //   console.table(info.directory);
+  
+
+const silver = businessData.directory.filter((item) => {
+    return item.level == "silver" || item.level =="gold";
+}
+);
+console.log(silver)
+
+}
+
+
+
+
+  fetchAndRenderDirectory();
+  
+  const renderDirectory = (directory) => {
+    const cards = document.querySelector("div.cards"); //selects output container element
+  
+    directory.forEach((directory) => {
+      //Create elements to add to div.cards element
+  
+      let card = document.createElement("section");
+      let h3 = document.createElement("h3");
+      let address = document.createElement("p");
+      let phone = document.createElement("p");
+      let website = document.createElement("p");
+      let logos = document.createElement("img");
+  
+      h3.textContent = `${directory.business}`;
+      address.textContent = `${directory.address}`;
+      phone.textContent = `${directory.phone}`;
+      website.textContent = `${directory.website}`;
+  
+      //Build the image logo by setting all the relevant attribute
+      logos.setAttribute("src", directory.imageurl);
+      logos.setAttribute("alt", `business logo for ${directory.business}`);
+      logos.setAttribute("loading", "lazy");
+      logos.setAttribute("width", "150");
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h3);
+      card.appendChild(logos);
+      card.appendChild(address);
+      card.appendChild(website);
+  
+      cards.appendChild(card);
+    }); //end of forEach loop
+  }; //  end of function expression
+  
