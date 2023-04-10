e5298bff6914ddbd5cfb6c0ba1a1a00402b1bd72
shirlcws <shirlcws@gmail.com>
2023-04-09 20:48:49 -0600

    Saving to test alignment in phone

--- project/script/forecast.js
+++ project/script/forecast.js
@@ -1,35 +1,53 @@
-//select an html element in the document
-const forecastTemp = document.querySelector("#forecast-temp");
-//use an apiurl to fetch a response from openweathermap.org through MediaQueryList
-async function apiFetch() {
-  const apiURL = `https://api.openweathermap.org/data/2.5/forecast?/q=Blackfoot&units=imperial&APPID=76cce6c470ec11e978662301c7f58bae&cnt=3`;
-}
+https://api.openweathermap.org/data/2.5/forecast?lat=32.4207&lon=-104.2288&appid=-104.2288&units=imperial&APPID=76cce6c470ec11e978662301c7f58bae
 
-//test script in console
-// try {
-const response = await fetch(apiURL);
-if (response.ok) {
-  const data = await response.json();
+lat=32.4207&lon=-104.2288
 
-  console.log(data); // this is for testing the call
-  //displayResults(data);
-} else {
-  throw Error(await response.text());
-}
-//display error if response is not ok
 
-//display results
 
-apiFetch();
-function displayResults(forecastData) {
-  forecastTemp.innerHTML = `<strong>${forecastData.main.temp.toFixed(
-    0
-  )}</strong>`;
 
-  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  const wDesc = weatherData.weather[0].description;
 
-  weatherIcon.setAttribute("src", iconsrc);
-  weatherIcon.setAttribute("alt", wDesc);
-  captionDesc.textContent = wDesc;
-}
+
+
+
+
+
+
+
+
+// //select an html element in the document
+// const forecastTemp = document.querySelector("#forecast-temp1");
+// const forecastTemp = document.querySelector("#forecast-temp2");
+// const forecastTemp = document.querySelector("#forecast-temp3");
+// //use an apiurl to fetch a response from openweathermap.org through MediaQueryList
+// async function apiFetch() {
+//   const apiURL = `https://api.openweathermap.org/data/2.5/forecast?/q=Blackfoot&units=imperial&APPID=76cce6c470ec11e978662301c7f58bae&cnt=3`;
+// }
+
+// //test script in console
+// // try {
+// const response = await fetch(apiURL);
+// if (response.ok) {
+//   const data = await response.json();
+
+//   console.log(data); // this is for testing the call
+//   //displayResults(data);
+// } else {
+//   throw Error(await response.text());
+// }
+// //display error if response is not ok
+
+// //display results
+
+// apiFetch();
+// function displayResults(forecastData) {
+//   forecastTemp.innerHTML = `<strong>${forecastData.main.temp.toFixed(
+//     0
+//   )}</strong>`;
+
+//   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+//   const wDesc = weatherData.weather[0].description;
+
+//   weatherIcon.setAttribute("src", iconsrc);
+//   weatherIcon.setAttribute("alt", wDesc);
+//   captionDesc.textContent = wDesc;
+// }
--- project/script/menu.js
+++ project/script/menu.js
@@ -1,12 +1,41 @@
-const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
-async function fetchAndRenderMenu() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.table(data.fruit);
-    displayFruits(data.fruit);
-}
-fetchAndRenderMenu();
+//connects to json file
+const Url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+//
+//asynchronously uses async function to fetch data from JSON file
+async function fetchAndRenderFruit() {
+  let response = await fetch(Url);
+  let data = await response.json();
 
-const displayFruit = (fruit) => {
-    const cards = document.querySelector("div.cards");
+  // console.table(data.fruit);
+  displayFruits(data.fruits);
 }
+fetchAndRenderFruits();
+
+const displayFruits = (fruits) => {
+  const cards = document.querySelector("div.cards"); //selects an output container element
+
+fruits.forEach((fruit) => {
+    //Create elements to add to div.cards element
+    let card = document.createElement("section");
+    let name = document.createElement("h2")
+    let id = document.createElement("p");
+    let nutritions = document.createElement("p");
+
+    // Build h2 content to show 7 fields, fruit name, id, nutrition.
+    h2.textContent = `${fruit.name} ${fruit.id} ${fruit.nutritions}`;
+    fruit.nutritions = document.createElement("carbohydrates, protein, fat, calories, sugar");
+    p.nutitions = document.createElement("protein");
+    p.nutritions = document.createElement(fat);
+    p.nutritions = document.createElement(calories);
+    p.nutritions = document.createElement(sugar);
+// Append the section(card) with the created elements
+card.appendChild(name);
+card.appendChild(id);
+card.appendChild(nutrition);
+
+cards.appendChild(card);
+}); // end of forEach loop
+
+
+}; // end of function expression
+
